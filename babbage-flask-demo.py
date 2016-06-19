a91a0635bcf9969891c97e725907ab7296b96b9f
cyberbikepunk <loic.jounot@gmail.com>
2016-06-19 18:22:58 +0200

    Initial commit.

---
+++ babbage-flask-demo.py
@@ -0,0 +1,23 @@
+from os import path
+from flask import Flask
+from os.path import join
+from sqlalchemy import create_engine
+from babbage.manager import JSONCubeManager
+from babbage.api import configure_api
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask('demo')
+engine = create_engine('sqlite://')
+app.debug = True
+# set a 'SECRET_KEY' to enable the Flask session cookies
+app.config['SECRET_KEY'] = '<replace with a secret key>'
+
+models_directory = path.dirname(join(__file__, 'models'))
+manager = JSONCubeManager(engine, models_directory)
+blueprint = configure_api(app, manager)
+app.register_blueprint(blueprint, url_prefix='/api/babbage')
+toolbar = DebugToolbarExtension(app)
+
+
+if __name__ == '__main__':
+    app.run()
